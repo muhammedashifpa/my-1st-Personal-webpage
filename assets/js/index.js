defd0e361569f2901e58785fd0a19e81b795c31d
muhammed ashif <ashifmohammed231@gmail.com>
2021-08-20 12:50:45 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,180 @@
+
+/*==============    variables ===============*/
+var nameState = false;
+var secNameTry = false;
+var numberState = false;
+var secNumberTry = false;
+var emailState = false;
+var secEmailTry = false;
+var messageState = true;
+/*==================================       name validation      =======================================*/
+/*==============    restric unwanted elements ===============*/
+$('#Inputfullname').on('keypress' ,function(event){
+    var x = event.which || event.keycode;
+    let valueBox = $('#Inputfullname').val();
+    /*=====    restric first space ====*/
+    if(valueBox.length == 0 && x === 32){
+      return false
+    }
+    if((x>=65&&x<=90)||(x>=97&&x<=122)||x===32){
+      /*======= make true after false by length ========*/
+      if(secNameTry&&valueBox.length>=5){
+        nameState=true;
+        $('#nameLabel').css("color", "var(--text-color)");
+      }
+      return true
+    }
+    else{
+      return false
+    }
+})
+
+/*=============  length lesstan 5  ============*/
+$('#Inputfullname').blur(function(){
+  let valueBox = $('#Inputfullname').val();
+  if(valueBox.length<5){
+    console.log('minimum 5 char');
+    nameState= false;
+    secNameTry=true;
+    $('#nameLabel').css("color", "red");
+  }
+})
+/*=============  autofill  ============*/
+$('#Inputfullname').change(function(){
+  let valueBox = $('#Inputfullname').val();
+  if(valueBox.length>5){
+    console.log('name auto filled');
+    nameState= true;
+    $('#nameLabel').css("color", "var(--text-color)");
+  }
+})
+
+/*========================================       number validation      =======================================*/
+$('#InputNumber').on('keypress' ,function(event){
+  var x = event.which || event.keycode;
+  let valueBox = $('#InputNumber').val();
+  if(secNumberTry&&valueBox.length==9){
+    $('#numberLabel').css("color", "var(--text-color)");
+  }
+  /*===== allow number only ====*/
+  if((x>=48&&x<=57)){
+    return true
+  }
+  else{
+    return false
+  }
+})
+
+$('#InputNumber').blur(function(){
+  let valueBox = $('#InputNumber').val();
+  if(valueBox.length<10){
+    console.log('minimum 10 num');
+    $('#numberLabel').css("color", "red");
+    numberState = false;
+    secNumberTry = true;
+  }
+})
+/*=============  autofill  ============*/
+$('#InputNumber').change(function(){
+  let valueBox = $('#InputNumber').val();
+  if(valueBox.length<10){
+    console.log('minimum 10 num');
+    $('#numberLabel').css("color", "red");
+    numberState = false;
+    secNumberTry = true;
+  }
+  else{
+    $('#numberLabel').css("color", "var(--text-color)");
+    numberState = true;
+  }
+})
+/*========================================       email validation      =======================================*/
+$('#InputEmail').blur(function(){
+  let valueBox = $('#InputEmail').val();
+  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(emailRegex.test(valueBox)){
+    console.log('@gmail.com');
+    emailState=true;
+    $('#labelEmail').css("color", "var(--text-color)");
+  }
+  else{
+    secEmailTry=true;
+    emailState=false;
+    $('#labelEmail').css("color", "red");
+  }
+})
+$('#InputEmail').keyup(function(){
+  if(secEmailTry){
+  let valueBox = $('#InputEmail').val();
+  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(emailRegex.test(valueBox)){
+    console.log('@gmail.com');
+    emailState=true;
+    $('#labelEmail').css("color", "var(--text-color)");
+  }
+  else{
+    emailState=false;
+    $('#labelEmail').css("color", "red");
+  }
+}
+})
+/*=============  autofill  ============*/
+$('#InputEmail').change(function(){
+  console.log('autofill')
+  let valueBox = $('#InputEmail').val();
+  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(emailRegex.test(valueBox)){
+    console.log('@gmail.com');
+    emailState=true;
+    $('#labelEmail').css("color", "var(--text-color)");
+  }
+  else{
+    secEmailTry=true;
+    emailState=false;
+    $('#labelEmail').css("color", "red");
+  }
+})
+/*========================================       submission      =======================================*/
+
+
+
+/*========================================       Ajax      =======================================*/
+$("#gform").submit((e)=>{
+  e.preventDefault()
+  if(!nameState){
+    $('#nameLabel').css("color", "red");
+  }
+  if(!numberState){
+    $('#numberLabel').css("color", "red");
+  }
+  if(!emailState){
+    $('#labelEmail').css("color", "red");
+  }
+  if(nameState&&numberState&&emailState){
+  $.ajax({
+      url:"https://script.google.com/macros/s/AKfycbyssVG5NPkyP_uVbtuE0GRKvVGBwFg8-gOsvc21FihhQbQMRtFA4In_C8HdfP0AqHrBHg/exec",
+      data:$("#gform").serialize(),
+      method:"post",
+      success:function (response){
+          alert("Form submitted successfully")
+          e.preventDefault();
+          $('#gform').trigger("reset");
+          nameState = false;
+          secNameTry = false;
+          numberState = false;
+          secNumberTry = false;
+          emailState = false;
+          secEmailTry = false;
+          messageState = true;
+          //window.location.href="https://google.com"
+      },
+      error:function (err){
+          alert("Something Error")
+
+      }
+  })
+}
+else{
+  alert('please fill the form')
+}
+})
